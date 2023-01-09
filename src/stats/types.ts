a6b9e74b801e336e9976142f596964cbbb00e21e
laireyx <laireyx@gmail.com>
2023-01-09 15:49:30 +0900

    기본 / 표기 / 표준 공격력 분리

--- src/stats/types.ts
+++ src/stats/types.ts
@@ -2,11 +2,22 @@ const PlayerStatName = {
   maxHp: "최대 HP",
   crit: "치명",
 
-  phyAtkBase: "물리 공격력",
-  fireAtkBase: "화염 공격력",
-  iceAtkBase: "얼음 공격력",
-  thunderAtkBase: "번개 공격력",
-  superpowerAtkBase: "이능 공격력",
+  phyAtkDefault: "물리 공격력(표준)",
+  fireAtkDefault: "화염 공격력(표준)",
+  iceAtkDefault: "얼음 공격력(표준)",
+  thunderAtkDefault: "번개 공격력(표준)",
+
+  phyAtkBase: "물리 공격력(기본)",
+  fireAtkBase: "화염 공격력(기본)",
+  iceAtkBase: "얼음 공격력(기본)",
+  thunderAtkBase: "번개 공격력(기본)",
+  superpowerAtkBase: "이능 공격력(기본)",
+
+  phyAtk: "물리 공격력(표기)",
+  fireAtk: "화염 공격력(표기)",
+  iceAtk: "얼음 공격력(표기)",
+  thunderAtk: "번개 공격력(표기)",
+  superpowerAtk: "이능 공격력(표기)",
 
   phyDef: "물리 내성",
   fireDef: "화염 내성",
